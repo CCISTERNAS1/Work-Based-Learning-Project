ed3a8e885c4cacec44aa7380a3fca02b20f5b760
IanCist <iantcisternas@gmail.com>
2025-05-14 09:51:59 -0400

    updated bootstrap links

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
   const navbar = document.querySelector(".techy-navbar");
 
-  // Hide the preloader after a short delay
+  
   setTimeout(() => {
     preloader.classList.add("hidden");
 
-    // Allow scrolling again
+    
     document.body.classList.remove("preload");
 
-    // Animate the navbar in
+   
     if (navbar) {
       navbar.classList.add("show-navbar");
     }
-  }, 800); // Adjust delay if needed
+  }, 800); 
 });
