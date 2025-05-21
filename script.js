dbc197de7fc6302f5cce61509dcbc0f514910698
IanCist <iantcisternas@gmail.com>
2025-05-21 11:38:02 -0400

    navbar dissapeared on other pages fixed

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
-  const navbar = document.querySelector("nav"); 
+  const navbar = document.querySelector(".navbar");
 
+  
   setTimeout(() => {
     preloader.classList.add("hidden");
+
+    
     document.body.classList.remove("preload");
 
+   
     if (navbar) {
-      navbar.classList.add("navbar-slide-down");
+      navbar.classList.add("show-navbar");
     }
   }, 800); 
 });
+
