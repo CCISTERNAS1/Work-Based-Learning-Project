558de62fc0e0e8d0c7c7314b5ace4e99ab9a61f7
IanCist <iantcisternas@gmail.com>
2025-05-21 11:31:19 -0400

    fixed some issues with animations and restyled about page

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
-  const navbar = document.querySelector(".techy-navbar");
+  const navbar = document.querySelector("nav"); 
 
-  
   setTimeout(() => {
     preloader.classList.add("hidden");
-
-    
     document.body.classList.remove("preload");
 
-   
     if (navbar) {
-      navbar.classList.add("show-navbar");
+      navbar.classList.add("navbar-slide-down");
     }
   }, 800); 
 });
-
